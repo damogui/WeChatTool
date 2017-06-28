2b454fe9c958e5ae4f0ef94ba24841dfa5d07d09
郭佳 <guo_jia_jia@hotmail.com>
2017-06-28 14:49:14 +0800

    进行校验

--- WeChatTool/Controllers/HomeController.cs
+++ WeChatTool/Controllers/HomeController.cs
@@ -37,9 +37,9 @@ namespace WeChatTool.Controllers
         /// </summary>
         /// <returns></returns>
 
-        public  ActionResult  WechatUpload()
+        public ActionResult WechatUpload()
         {
-            JsonResult jsResult = new JsonResult() { JsonRequestBehavior= JsonRequestBehavior.AllowGet };
+            JsonResult jsResult = new JsonResult() { JsonRequestBehavior = JsonRequestBehavior.AllowGet };
             Stream uploadStream = null;
             FileStream fs = null;
             try
@@ -52,7 +52,7 @@ namespace WeChatTool.Controllers
                 int contentLen = 0;
 
                 string fileName = Path.GetFileName(postFileBase.FileName);
-                if (postFileBase.FileName==""|| uploadStream.Length==0)
+                if (postFileBase.FileName == "" || uploadStream.Length == 0)
                 {
                     jsResult.Data = "请选择文件之后再上传";
                     return jsResult;
@@ -70,9 +70,9 @@ namespace WeChatTool.Controllers
 
                 //保存页面数据，上传的文件只保存路径  
                 string productImage = "/Product/" + fileName;//保存路径
-               
 
-              
+
+
             }
             catch (Exception ex)
             {
@@ -89,10 +89,12 @@ namespace WeChatTool.Controllers
                     uploadStream.Close();
                 }
             }
-           
+
             jsResult.Data = 1;
             jsResult.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
-            return  View("SetConfig");
+
+            RedirectToAction("SetConfig");
+            return View("SetConfig");
 
 
         }
@@ -106,13 +108,27 @@ namespace WeChatTool.Controllers
         /// <returns></returns>
         public JsonResult SetOrgWeChat(OrgWeChat orgWeChat)
         {
-            JsonResult js=new JsonResult() {JsonRequestBehavior = JsonRequestBehavior.AllowGet};
-            string data = Request.Form.Get("data") ??"[]";
+            JsonResult js = new JsonResult() { JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            string data = Request.Form.Get("data") ?? "[]";
             //OrgStuClass para = JsonConvert.DeserializeObject<OrgStuClass>(data);
+            int num = IsExit(orgWeChat.OrgId);//是否存在
+            if (num > 0)
+            {
+                js.Data = "已经存在";
+                return js;
+            }
+            int num1 = IsTemplantSafe(orgWeChat);//是否存在
+            if (num1>0)
+            {
+                js.Data = "模板id不能相同";
+                return js;
+            }
+
+            //添加校验
             string sqlOrg =
                 "insert into `mfg_wechat_org` (`OrgID`, `ToUserID`, `AppID`, `AppSecret`, `IsSys`, `OrgUrl`, `IsEnable`, `QRCode`, `OrgMessage`, `CreateTime`) values(@OrgId,@ToUserID,@AppID,@AppSecret,'','www.mofangge.net','','0',@OrgMessage,now());insert into `mfg_wechat_template` (`OrgID`, `TemplateId`, `TemplateType`, `Messages`, `Remark`, `CreateTime`) values(@OrgId,@AddCourseId,'1','报课','报课',NOW());insert into `mfg_wechat_template` (`OrgID`, `TemplateId`, `TemplateType`, `Messages`, `Remark`, `CreateTime`) values(@OrgId, @CutCourseId, '2', '下课', '下课', NOW());insert into `mfg_wechat_template` (`OrgID`, `TemplateId`, `TemplateType`, `Messages`, `Remark`, `CreateTime`) values(@OrgId, @LogMsgId, '3', '监督提醒', '监督提醒',NOW());insert into `mfg_wechat_template` (`OrgID`, `TemplateId`, `TemplateType`, `Messages`, `Remark`, `CreateTime`) values(@OrgId, @BackCourseId, '4', '退课', '退课', NOW());";
 
-            List<MySqlParameter>listPar=new List<MySqlParameter>();
+            List<MySqlParameter> listPar = new List<MySqlParameter>();
             listPar.Add(new MySqlParameter("@OrgId", orgWeChat.OrgId));
             listPar.Add(new MySqlParameter("@ToUserID", orgWeChat.BegId));
             listPar.Add(new MySqlParameter("@AppID", orgWeChat.AppId));
@@ -123,17 +139,57 @@ namespace WeChatTool.Controllers
             listPar.Add(new MySqlParameter("@CutCourseId", orgWeChat.CutCourseId));
             listPar.Add(new MySqlParameter("@LogMsgId", orgWeChat.LogMsgId));
             listPar.Add(new MySqlParameter("@BackCourseId", orgWeChat.BackCourseId));
-
-
-
-          int result=DBHelper.ExecSql(sqlOrg,listPar.ToArray());//执行添加
+            int result = DBHelper.ExecSql(sqlOrg, listPar.ToArray());//执行添加
             js.Data = result;
 
             return js;
 
         }
+        /// <summary>
+        /// 判断是不是存在两个模板id值相等
+        /// </summary>
+        /// <param name="orgWeChat"></param>
+        /// <returns></returns>
+        private int IsTemplantSafe(OrgWeChat orgWeChat)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add(orgWeChat.AddCourseId, "报课");
+            if (dic.ContainsKey(orgWeChat.CutCourseId))
+            {
+
+                return 1;//模板id不能相同
+            }
+            dic.Add(orgWeChat.CutCourseId, "下课");
+            if (dic.ContainsKey(orgWeChat.BackCourseId))
+            {
+
+                return 1;//模板id不能相同
+            }
+            dic.Add(orgWeChat.BackCourseId, "退课");
+            if (dic.ContainsKey(orgWeChat.LogMsgId))
+            {
+
+                return 1;//模板id不能相同
+            }
+            dic.Add(orgWeChat.LogMsgId, "登录");
+
+            return 0;
+
+        }
+
+        /// <summary>
+        /// 是否存在
+        /// </summary>
+        /// <param name="orgId"></param>
+        /// <returns></returns>
+        private int IsExit(int orgId)
+        {
+
+            string sqlSearch = "SELECT  COUNT(1)   FROM  mfg_wechat_org    WHERE  orgId=@orgId";
 
 
+            return Convert.ToInt32(DBHelper.GetScalarFile(sqlSearch, new MySqlParameter("@orgId", orgId)));
 
+        }
     }
 }
